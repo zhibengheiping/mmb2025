dcf9b3bc4440500124b225d5a6bd3acb3ea0ad7e
github-action-benchmark <github@users.noreply.github.com>
2025-11-21 09:28:02 +0000

    add MiniMoonbit2025 Benchmark (customBiggerIsBetter) benchmark result for 1e659752feb7a4c69edd2c6689bc08687a45639d

---
+++ dev/bench/data.js
@@ -0,0 +1,107 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1763717282244,
+  "repoUrl": "https://github.com/zhibengheiping/mmb2025",
+  "entries": {
+    "MiniMoonbit2025 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zhibengheiping@163.com",
+            "name": "直崩黑屏",
+            "username": "zhibengheiping"
+          },
+          "committer": {
+            "email": "zhibengheiping@163.com",
+            "name": "直崩黑屏",
+            "username": "zhibengheiping"
+          },
+          "distinct": true,
+          "id": "1e659752feb7a4c69edd2c6689bc08687a45639d",
+          "message": "Initial commit",
+          "timestamp": "2025-11-21T17:08:09+08:00",
+          "tree_id": "284d585a9ae5573a986b3bb5bff94e4aa7a91a86",
+          "url": "https://github.com/zhibengheiping/mmb2025/commit/1e659752feb7a4c69edd2c6689bc08687a45639d"
+        },
+        "date": 1763717281350,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "conv_pool3",
+            "value": 576.2473122233598,
+            "unit": "Percent"
+          },
+          {
+            "name": "queen",
+            "value": 132.72690765284423,
+            "unit": "Percent"
+          },
+          {
+            "name": "lisp",
+            "value": 47.15613942170525,
+            "unit": "Percent"
+          },
+          {
+            "name": "almabench",
+            "value": 131.8482934356185,
+            "unit": "Percent"
+          },
+          {
+            "name": "eigen",
+            "value": 1132.5222626363973,
+            "unit": "Percent"
+          },
+          {
+            "name": "qr",
+            "value": 1552.3107805302882,
+            "unit": "Percent"
+          },
+          {
+            "name": "svd",
+            "value": 2568.5278664881034,
+            "unit": "Percent"
+          },
+          {
+            "name": "nbody",
+            "value": 200.42321717941115,
+            "unit": "Percent"
+          },
+          {
+            "name": "invmat",
+            "value": 4847.097032385354,
+            "unit": "Percent"
+          },
+          {
+            "name": "lu",
+            "value": 1841.9636263501857,
+            "unit": "Percent"
+          },
+          {
+            "name": "cholesky",
+            "value": 1239.8778100717877,
+            "unit": "Percent"
+          },
+          {
+            "name": "fpquicksort",
+            "value": 52.63151146017103,
+            "unit": "Percent"
+          },
+          {
+            "name": "conv_pool2",
+            "value": 182.91882538271068,
+            "unit": "Percent"
+          },
+          {
+            "name": "schur",
+            "value": 2003.8232027557758,
+            "unit": "Percent"
+          },
+          {
+            "name": "avl",
+            "value": 102.96179238928829,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
